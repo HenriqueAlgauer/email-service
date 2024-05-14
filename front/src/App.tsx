fb449a088bb6e61e879a4ed79ade8a7899059751
henriqueAlgauer <algauer.henrique@gmail.com>
2024-05-14 14:41:55 -0300

    finalizado

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,62 +1,68 @@
-import { useState } from 'react'
-import './App.css'
-import PrimaryInput from './components/PrimaryInput/PrimaryInput'
-import { Button, Spacer } from '@chakra-ui/react'
-import { useIdentityMutation } from './hooks/userIdentityMutation'
+import { useState } from "react";
+import "./App.css";
+import PrimaryInput from "./components/PrimaryInput/PrimaryInput";
+import { Button, Spacer } from "@chakra-ui/react";
+import { useIdentityMutation } from "./hooks/userIdentityMutation";
 
 function App() {
-  const { mutate } = useIdentityMutation()
-  const [email, setEmail] = useState("")
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setlastName] = useState("")
+  const { mutate } = useIdentityMutation();
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setlastName] = useState("");
 
-  const submit = () =>{
+  const submit = () => {
     mutate({
-      email,
       firstName,
-      lastName
-    })
-  }
+      lastName,
+      email,
+    });
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <form>
-        <div className='name-form-container'>
+        <div className="name-form-container">
           <PrimaryInput
             value={firstName}
-            onChange={event => setFirstName(event.target.value)}
-            name='firstName'
-            label='Nome'
-            placeholder='João'
+            onChange={(event) => setFirstName(event.target.value)}
+            name="firstName"
+            label="Nome"
+            placeholder="João"
           />
           <PrimaryInput
             value={lastName}
-            onChange={event => setlastName(event.target.value)}
-            name='lastName'
-            label='Sobrenome'
-            placeholder='Silva'
+            onChange={(event) => setlastName(event.target.value)}
+            name="lastName"
+            label="Sobrenome"
+            placeholder="Silva"
           />
         </div>
-        <Spacer height='4' />
+        <Spacer height="4" />
         <PrimaryInput
           value={email}
-          onChange={event => setEmail(event.target.value)} name='email'
-          label='Digite seu e-mail'
-          placeholder='joaosilva@gmail.com'
+          onChange={(event) => setEmail(event.target.value)}
+          name="email"
+          label="Digite seu e-mail"
+          placeholder="joaosilva@gmail.com"
         />
-        <Spacer height='4' />
-        <Button colorScheme='green' width='100%' onClick={submit} >ENVIAR</Button>
+        <Spacer height="4" />
+        <Button colorScheme="green" width="100%" onClick={submit}>
+          ENVIAR
+        </Button>
       </form>
-      <Spacer maxWidth='6'/>
+      <Spacer maxWidth="6" />
       <div className="product-details">
         <h2>Assinatura mensal</h2>
-        <Spacer height='4'/>
+        <Spacer height="4" />
         <p>Você irá pagar </p>
         <span>R$ 250,00</span>
-        <p>Regras: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia distinctio nostrum</p>
+        <p>
+          Regras: Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Mollitia distinctio nostrum
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- front/src/hooks/userIdentityMutation.ts
+++ front/src/hooks/userIdentityMutation.ts
@@ -1,21 +1,21 @@
-import { useMutation } from "react-query"
-import { PersonData } from "../interface/person-data"
-import axios from "axios"
+import { useMutation } from "react-query";
+import { PersonData } from "../interface/person-data";
+import axios from "axios";
 
-const postData = (data: PersonData) =>{
-    return axios.post('https://localhost:8080/send-data', data)
+const postData = (data: PersonData) => {
+  return axios.post("https://localhost:3200/send", data);
+};
+
+function useIdentityMutation() {
+  const { mutate, isLoading, isSuccess } = useMutation({
+    mutationFn: (data: PersonData) => postData(data),
+  });
+
+  return {
+    mutate,
+    isLoading,
+    isSuccess,
+  };
 }
 
-function useIdentityMutation(){
-    const {mutate, isLoading, isSuccess} = useMutation({
-        mutationFn: (data: PersonData) => postData(data)
-    })
-
-    return {
-        mutate,
-        isLoading,
-        isSuccess
-    }
-}
-
-export {useIdentityMutation}
+export { useIdentityMutation };
